59031bb18c49ccea8bcfca2054c89b117cd883dd
Gaya Kessler <gaya.kessler@gmail.com>
2016-06-07 15:08:16 +0200

    add loader

--- react-no-redux/components/contacts.js
+++ react-no-redux/components/contacts.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
 export default React.createClass({
+  getInitialState() {
+    return {
+      contacts: [],
+      loading: true,
+    };
+  },
+
+  list() {
+    if (this.state.loading) {
+      return <div className='progress'>
+        <div className='progress-bar progress-bar-info progress-bar-striped active'
+             style={{ width: '100%', }} />
+      </div>;
+    }
+
+    return <section>Contacts list</section>;
+  },
+
   render() {
-    return this.props.children || <section>Contacts list</section>;
+    return this.props.children || this.list();
   },
 });
