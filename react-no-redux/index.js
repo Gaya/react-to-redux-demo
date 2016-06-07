4e531b977307a483aa232eb3e58df85ff49ac0bd
Gaya Kessler <gaya.kessler@gmail.com>
2016-06-07 14:40:20 +0200

    basics

--- react-no-redux/index.js
+++ react-no-redux/index.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 
 const App = props => (
   <section>
-    <h1>Contacts in React!</h1>
+    <h1>Contacts in React</h1>
     { props.children }
   </section>
 );
 
-const About = () => <section>About</section>;
-const Users = props => props.children || <section>Users</section>;
-const User = () => <section>I am a User</section>;
+const Contacts = props => props.children || <section>Contacts list</section>;
+
+const Contact = () => <section>I am a contact!</section>;
+
 const NoMatch = () => <section>Sorry, page not found!</section>;
 
 render((
   <Router history={browserHistory}>
     <Route path="/" component={App}>
-      <Route path="about" component={About}/>
-      <Route path="users" component={Users}>
-        <Route path="/user/:userId" component={User}/>
+      <IndexRoute component={Contacts}/>
+      <Route path="contacts" component={Contacts}>
+        <Route path="/contact/:contactId" component={Contact}/>
       </Route>
       <Route path="*" component={NoMatch}/>
     </Route>
