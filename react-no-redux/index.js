3e2d74ccf0ec8215afb56870908eca994194dd02
Gaya Kessler <gaya.kessler@gmail.com>
2016-06-07 14:36:32 +0200

    basic routing setup

--- react-no-redux/index.js
+++ react-no-redux/index.js
@@ -1,1 +1,27 @@
-console.log('whatsup');
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+
+const App = props => (
+  <section>
+    <h1>Contacts in React!</h1>
+    { props.children }
+  </section>
+);
+
+const About = () => <section>About</section>;
+const Users = props => props.children || <section>Users</section>;
+const User = () => <section>I am a User</section>;
+const NoMatch = () => <section>Sorry, page not found!</section>;
+
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <Route path="about" component={About}/>
+      <Route path="users" component={Users}>
+        <Route path="/user/:userId" component={User}/>
+      </Route>
+      <Route path="*" component={NoMatch}/>
+    </Route>
+  </Router>
+), document.body);
