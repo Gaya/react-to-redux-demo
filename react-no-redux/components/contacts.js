75cc9715a8c2a59f08c2bfa4ab88cdccf7efba3c
Gaya Kessler <gaya.kessler@gmail.com>
2016-06-07 15:17:05 +0200

    get contacts from json

--- react-no-redux/components/contacts.js
+++ react-no-redux/components/contacts.js
@@ -8,12 +8,29 @@ export default React.createClass({
     };
   },
 
+  componentDidMount() {
+    fetch('/contacts.json')
+      .then((response) => response.json())
+      .then(this.setContactsResult);
+  },
+
+  setContactsResult(contacts) {
+    this.setState({
+      loading: false,
+      contacts,
+    });
+  },
+
   list() {
     if (this.state.loading) {
       return <div className='progress'>
         <div className='progress-bar progress-bar-info progress-bar-striped active'
              style={{ width: '100%', }} />
       </div>;
+    }
+
+    if (this.state.contacts.length === 0) {
+      return <section>No contacts found.</section>;
     }
 
     return <section>Contacts list</section>;
