112968bf32465a7cdbeca8a730632b4d5d0405ca
Gaya Kessler <gaya.kessler@gmail.com>
2016-06-08 23:11:25 +0200

    twitter accounts implementation

--- react-redux/components/twitter-accounts.js
+++ react-redux/components/twitter-accounts.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const TwitterAccount = () => (null);
+const TwitterAccount = ({ accounts = [] }) => (
+  <section>
+    <h2>Contacts with a Twitter Account</h2>
 
-export default TwitterAccount;
+    {accounts.map(account => <span>{account}, </span>)}
+  </section>
+);
+
+function mapStateToProp(state) {
+  return {
+    accounts: state.contacts.items
+      .filter(contact => contact.twitter !== '')
+      .map(contact => contact.twitter),
+  };
+}
+
+export default connect(
+  mapStateToProp,
+)(TwitterAccount);
